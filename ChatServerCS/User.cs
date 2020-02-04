26f563f5cbd5ce6bc5328497fa0a92e901bd0c10
kabinad teshager <kabinad.teshager@hotmail.com>
2020-02-04 14:28:40 +0200

    initial commit

---
+++ ChatServerCS/User.cs
@@ -0,0 +1,15 @@
+﻿namespace ChatServerCS
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string ID { get; set; }
+        public byte[] Photo { get; set; }
+    }
+
+    public class SoftwareInstance
+    {
+        public string Name { get; set; }
+        public string ID { get; set; }       
+    }
+}
